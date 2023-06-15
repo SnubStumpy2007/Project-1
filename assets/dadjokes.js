3ee31b4675933c389e0191601bdaefacdd31c709
SnubStumpy2007 <joshua192007@gmail.com>
2023-06-14 21:06:20 -0400

    update index.html

---
+++ assets/dadjokes.js
@@ -0,0 +1,15 @@
+const getDadJoke = async function()  {
+  const dadJokeResponse = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+          Accept: "application/json"
+      }
+  });
+  const dadJokeJSON = await dadJokeResponse.json();
+  if (dadJokeJSON.status === 200) {
+      return dadJokeJSON.joke;
+  } else {
+      return "Unfortunately I cannot return a Dad joke at this time!"
+  }
+};
+
+(async () => console.log(await getDadJoke()))();
